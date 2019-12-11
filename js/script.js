c462112ad23953a8556cc79b0379aef5a71941f3
joe07 <joew4119@gmail.com>
2019-12-11 20:37:55 +0000

    update

--- js/script.js
+++ js/script.js
@@ -2,30 +2,32 @@ const app = new Vue({
     el: "#app",
     data: { predictions: [], stationName: '' },
     mounted() {
-        get_times();
-
         axios.interceptors.request.use(function(config) {
+            $('#modal-1').toggleClass("md-show"); //you can list several class names
 
-            app.predictions = [];
-            app.stationName = '';
             return config;
-
         }, function(error) {
-          // Do something with request error
-          console.log('Error');
           return Promise.reject(error);
         });
-    },
 
+        axios.interceptors.response.use(function (response) {
+            $('#modal-1').toggleClass("md-show"); //you can list several class names
+          return response;
+        }, function (error) {
+          return Promise.reject(error);
+        });
+
+        get_times();
+    },
 });
 
 
 function get_times() {
-
-
-    axios.get("http://transportapi.com/v3/uk/bus/stop/43000312304/live.json?app_id=b3bcf524&app_key=954ba16e33264c93d56fd1cb79dc3d30")
+    axios.get("https://transportapi.com/v3/uk/bus/stop/43000312304/live.json?app_id=c3e7de7c&app_key=7e0ced32fcfcfc3afe87056d60680d74")
     .then(response => {
+
         app.stationName = response['data']['stop_name'];
+        app.predictions = [];
 
         let currentTime = moment(response['data']['request_time'], 'YYYY-MM-DDThh:mm:ss+00:00');
 
@@ -77,60 +79,3 @@ function get_times() {
     })
 }
 
-
-function get_timess() {
-    axios.get("https://cors-anywhere.herokuapp.com/http://api.tfwm.org.uk/StopPoint/43000312301/Arrivals?app_id=3f1c6cd7&app_key=7f46797c63d120abde6f83c36bbbf3d6&formatter=JSON")
-    .then(response => {
-        let time = '';
-        let api_data = response.data['Predictions']['Prediction'];
-
-        app.predictions = [];
-
-        for (let prediction in api_data) {
-            if (api_data.hasOwnProperty(prediction)) {
-
-                let timeToArrival = 0;
-                let timeToArrivalReadable = '';
-                let arrivalTime = '';
-                let live = false;
-
-                time = moment(api_data[prediction]['TimeToLive'], 'YYYY-MM-DDThh:mm:ss+01');
-
-                if (api_data[prediction]['ExpectedArrival'] !== '') {
-                    live = true;
-                    arrivalTime = moment(api_data[prediction]['ExpectedArrival'], 'YYYY-MM-DDThh:mm:ss+01');
-                } else {
-                    live = false;
-                    arrivalTime = moment(api_data[prediction]['ScheduledArrival'], 'YYYY-MM-DDThh:mm:ss+01');
-                }
-
-                timeToArrival = Math.round(moment.duration(arrivalTime - time).asMinutes());
-
-                if (timeToArrival > 1) {
-                    timeToArrivalReadable = timeToArrival.toString() + ' mins'
-                } else if (timeToArrival === 1) {
-                    timeToArrivalReadable = timeToArrival.toString() + ' min'
-                } else if (timeToArrival === 0) {
-                    timeToArrivalReadable = 'Due'
-                }
-
-                app.predictions.push({
-                    'id': api_data[prediction]['Id'],
-                    'bus': api_data[prediction]['LineName'].toUpperCase(),
-                    'destination': api_data[prediction]['Towards'],
-                    'timeToArrival': timeToArrival,
-                    'timeToArrivalReadable': timeToArrivalReadable,
-                    'arrivalTime': arrivalTime,
-                    'live': live
-                });
-
-
-                app.predictions.sort(function (a, b) {
-                    return a.arrivalTime - b.arrivalTime
-                });
-
-                app.stationName = api_data[prediction]["StationName"];
-            }
-        }
-    })
-}
